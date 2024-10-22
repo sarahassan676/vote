98c21c1c995487c8fb24393bf226d29a4cef7846
Sara Hassan <185543653+sarahassan676@users.noreply.github.com>
2024-10-22 09:58:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<html dir="rtl">
+	<head>
+		<title>المفوضية العامة للانتخابات</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="index.php"> المفوضية العامة للانتخابات </a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">الـرئـيـسـيـة</a></li>
+					<li><a href="gama.php">عـن الـتصويت </a></li>
+				<!--	<li><a href="shorot.php">اخر الاخبار</a></li> -->
+					<li><a href="admina.php">قائمة المرشحين</a></li>
+					<li><a href="msg.php">راسـلـنـا</a></li>
+					<li><a href="cool.php">اتـصـل بـنـا</a></li>
+					<li><a href="admin.php">لـوحـة الـتـحـكـم</a></li>
+				</ul>
+			</nav>
+
+		<!-- Main -->
+		<br>
+				<div class="container">
+
+					<header class="major special">
+						<h2>موقع للتصويت الالكتروني للانتخابات الرئاسية</h2>
+						<h2> المفوضية العامة للانتخابات </h2>
+					</header>
+					<a href="#" class="image fit"><img src="images/Sudan-Logo.png" width="400" height="300" alt="" /></a>
+				</div>
+				<div align="center">
+				 <button style="width=100px; height:50px;" onclick= "document.location=' admina.php'">صوت الان</button> 
+				</div>
+				<br>
+				<br>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled.</li>
+						<li>Images: <a href="http://unsplash.com">Unsplash</a>.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
